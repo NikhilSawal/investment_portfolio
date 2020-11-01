ffce3f73f85eab0431aca24970f95d0dd9af53d5
NikhilSawal <nikhil.sawal18@gmail.com>
2020-11-01 11:44:43 -0600

    Adds absolute and percentage delta values to s&p, nasdaq and dow with timestamp information in scraper.py script

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 import scrapy
+import datetime
 
 class IndexSpider(scrapy.Spider):
     name = "index"
@@ -9,7 +10,14 @@ class IndexSpider(scrapy.Spider):
     def parse(self, response):
         index = response.css("span.Trsdu\(0\.3s\)::text").getall()
         yield {
-            's&p_500' : index[0],
-            'dow_30'  : index[3],
-            'nasdaq'  : index[6],
+            'datetime'          : datetime.datetime.now().strftime("%Y-%m-%d %X"),
+            's&p_500'           : index[0],
+            's&p_500_delta'     : index[1],
+            's&p_500_delta(%)'  : index[2],
+            'dow_30'            : index[3],
+            'dow_30_delta'      : index[4],
+            'dow_30_delta(%)'   : index[5],
+            'nasdaq'            : index[6],
+            'nasdaq_delta'      : index[7],
+            'nasdaq_delta(%)'   : index[8],
         }
