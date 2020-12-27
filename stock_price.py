1c1e243a43d309600f0309fb454d4352eb4df404
NikhilSawal <nikhil.sawal18@gmail.com>
2020-12-27 16:12:14 -0600

    Add a spider file stock_price.py that gets stock prices for selected stocks

---
+++ stock_price.py
@@ -0,0 +1,33 @@
+import scrapy
+import datetime
+import helper_functions as hf
+
+
+hf.slack_msg("Start stock price scrape")
+
+class IndexSpider(scrapy.Spider):
+    name = "stock_price"
+    start_urls = ['https://finance.yahoo.com/quote/TWLO?p=TWLO',
+                  'https://finance.yahoo.com/quote/UAL?p=UAL',
+                  'https://finance.yahoo.com/quote/ZNGA?p=ZNGA',
+                  'https://finance.yahoo.com/quote/UBER?p=UBER',
+                  'https://finance.yahoo.com/quote/CHGG?p=CHGG',
+                  'https://finance.yahoo.com/quote/ETSY?p=ETSY',
+                  'https://finance.yahoo.com/quote/GOCO?p=GOCO',
+                  'https://finance.yahoo.com/quote/GOOGL?p=GOOGL',
+                  'https://finance.yahoo.com/quote/CMG?p=CMG',
+                  'https://finance.yahoo.com/quote/SHOP?p=SHOP',
+                  'https://finance.yahoo.com/quote/TSLA?p=TSLA'
+                  ]
+
+    def parse(self, response):
+        index = response.css('div.D\(ib\) span.Trsdu\(0\.3s\)::text').getall()
+        name = response.css('div.D\(ib\) h1.D\(ib\)::text').getall()
+        yield {
+            'datetime'  : datetime.datetime.now().strftime("%Y-%m-%d %X"),
+            'name'      : name[0],
+            'price'     : index[9],
+            'delta'     : index[10],
+        }
+
+hf.slack_msg("End stock price scrape")
