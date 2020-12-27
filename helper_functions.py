7c782ac45bd05939905edc4e40bf6cd3a7dc1b5e
NikhilSawal <nikhil.sawal18@gmail.com>
2020-12-27 16:11:32 -0600

    Adds method get_working_ip() to get a list of ips for rotating ip

--- helper_functions.py
+++ helper_functions.py
@@ -10,3 +10,21 @@ def slack_msg(msg):
 
     webhook = os.environ.get("SLACK_API_KEY")
     requests.post(webhook, json.dumps(data))
+
+
+def get_working_ip(proxy):
+
+    try:
+        r = requests.get('https://finance.yahoo.com/', proxies={'http':proxy, 'https':proxy}, timeout=randrange(7, 15))
+        print(r.json(), ' - working')
+    except:
+        print('Not working')
+    return proxy
+
+# get_working_ip('177.69.23.177:80')
+
+# with open('/Users/nikhilsawal/OneDrive/investment_portfolio/ip.json') as f:
+#     df = json.load(f)
+#
+# for index in range(len(df)):
+#     print(index, get_working_ip(str(df[index]['ip_address']) + ':' + str(df[index]['port'])))
