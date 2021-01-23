72ee26c6390952a5a174c867a9eb24f510082fc2
NikhilSawal <nikhil.sawal18@gmail.com>
2021-01-22 20:25:30 -0600

    test data

--- datafiles/company_profile.jl
+++ datafiles/company_profile.jl
@@ -75,3 +75,14 @@
 {"datetime": "2021-01-22 16:00:04", "name": "Chipotle Mexican Grill, Inc. (CMG)", "sector(s)": "Consumer Cyclical", "industry": "Restaurants", "employee_count": "94,000"}
 {"datetime": "2021-01-22 16:00:04", "name": "Tesla, Inc. (TSLA)", "sector(s)": "Consumer Cyclical", "industry": "Auto Manufacturers", "employee_count": "48,016"}
 {"datetime": "2021-01-22 16:00:04", "name": "Shopify Inc. (SHOP)", "sector(s)": "Technology", "industry": "Software\u2014Application", "employee_count": "5,000"}
+{"datetime": "2021-01-22 20:12:02", "name": "Alphabet Inc. (GOOGL)", "sector(s)": "Communication Services", "industry": "Internet Content & Information", "employee_count": "132,121"}
+{"datetime": "2021-01-22 20:12:02", "name": "United Airlines Holdings, Inc. (UAL)", "sector(s)": "Industrials", "industry": "Airlines", "employee_count": "74,400"}
+{"datetime": "2021-01-22 20:12:02", "name": "Zynga Inc. (ZNGA)", "sector(s)": "Communication Services", "industry": "Electronic Gaming & Multimedia", "employee_count": "1,883"}
+{"datetime": "2021-01-22 20:12:02", "name": "Uber Technologies, Inc. (UBER)", "sector(s)": "Technology", "industry": "Software\u2014Application", "employee_count": "21,600"}
+{"datetime": "2021-01-22 20:12:02", "name": "GoHealth, Inc. (GOCO)", "sector(s)": "Financial Services", "industry": "Insurance Brokers", "employee_count": "1,892"}
+{"datetime": "2021-01-22 20:12:02", "name": "Chegg, Inc. (CHGG)", "sector(s)": "Consumer Defensive", "industry": "Education & Training Services", "employee_count": "1,401"}
+{"datetime": "2021-01-22 20:12:02", "name": "Twilio Inc. (TWLO)", "sector(s)": "Communication Services", "industry": "Internet Content & Information", "employee_count": "3,664"}
+{"datetime": "2021-01-22 20:12:02", "name": "Etsy, Inc. (ETSY)", "sector(s)": "Consumer Cyclical", "industry": "Internet Retail", "employee_count": "1,374"}
+{"datetime": "2021-01-22 20:12:03", "name": "Tesla, Inc. (TSLA)", "sector(s)": "Consumer Cyclical", "industry": "Auto Manufacturers", "employee_count": "48,016"}
+{"datetime": "2021-01-22 20:12:03", "name": "Chipotle Mexican Grill, Inc. (CMG)", "sector(s)": "Consumer Cyclical", "industry": "Restaurants", "employee_count": "94,000"}
+{"datetime": "2021-01-22 20:12:03", "name": "Shopify Inc. (SHOP)", "sector(s)": "Technology", "industry": "Software\u2014Application", "employee_count": "5,000"}
