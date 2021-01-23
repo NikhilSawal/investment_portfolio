f504f390e1a5b3150d7a8fb6a002c929d4739185
NikhilSawal <nikhil.sawal18@gmail.com>
2021-01-22 22:54:31 -0600

    Adds comments to different methods in helper_functions.py script

--- helper_functions.py
+++ helper_functions.py
@@ -4,6 +4,11 @@ import os
 
 def slack_msg(msg):
 
+    """This method is called from different scripts (all spiders currently) to send
+    notification to slack about the completion of jobs. This script also sources the
+    SLACK_API_KEY, which is a global environment variable, that can be sourced from
+    the ./bashprofile in root directory and contab files."""
+
     data = {
         "text" : msg
     }
@@ -14,17 +19,15 @@ def slack_msg(msg):
 
 def get_working_ip(proxy):
 
+    """This method is not currently in operation, but will soon be put to use the
+    frequency of scrape needs to be increased to gather information on shorter time
+    spans. This method will try a set of ip's for the spider to scrape the given weblink
+    and would return ip's that work with the website, to avoid the system ip from getting
+    blocked."""
+
     try:
         r = requests.get('https://finance.yahoo.com/', proxies={'http':proxy, 'https':proxy}, timeout=randrange(7, 15))
         print(r.json(), ' - working')
     except:
         print('Not working')
     return proxy
-
-# get_working_ip('177.69.23.177:80')
-
-# with open('/Users/nikhilsawal/OneDrive/investment_portfolio/ip.json') as f:
-#     df = json.load(f)
-#
-# for index in range(len(df)):
-#     print(index, get_working_ip(str(df[index]['ip_address']) + ':' + str(df[index]['port'])))
