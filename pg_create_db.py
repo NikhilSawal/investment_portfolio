7bd3d45fac6e66e50a4c6ec9013e63c29cd0dc7a
NikhilSawal <nikhil.sawal18@gmail.com>
2021-01-26 14:35:46 -0600

    Adds scripts to create the postgres database to hold data and pipe data into this database

---
+++ pg_create_db.py
@@ -0,0 +1,120 @@
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+import os
+
+####################################
+# Get global keys
+pg_auth = os.environ.get("PG_AUTH")
+####################################
+
+###################
+# CREATE DATABASE #
+###################
+
+### Connect to PostgreSQL DBMS
+con = psycopg2.connect(host="localhost", user="postgres", password=pg_auth)
+con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+### Obtain a DB Cursor
+cursor = con.cursor()
+
+### Create DB
+sqlCreateDatabase = "CREATE DATABASE investment_db;"
+cursor.execute(sqlCreateDatabase)
+
+
+################
+# CREATE TABLE #
+################
+
+### Connect to PostgreSQL database
+postgresConnection = psycopg2.connect(host="localhost",
+                                      dbname="investment_db",
+                                      user="postgres",
+                                      password=pg_auth)
+
+cur = postgresConnection.cursor()
+
+## Create table for the stock indexes (S&P 500, Dow, NASDAQ)
+cur.execute("""
+                CREATE TABLE indexes(
+                id SERIAL PRIMARY KEY,
+                datetime DATE,
+                snp_500 NUMERIC,
+                snp_500_delta NUMERIC,
+                snp_500_delta_perc NUMERIC,
+                dow_30 NUMERIC,
+                dow_30_delta NUMERIC,
+                dow_30_delta_perc NUMERIC,
+                nasdaq NUMERIC,
+                nasdaq_delta NUMERIC,
+                nasdaq_delta_perc NUMERIC
+            )
+            """)
+
+### Create table for company profiles
+cur.execute("""
+               CREATE TABLE company_profile(
+               id SERIAL PRIMARY KEY,
+               date DATE,
+               name VARCHAR(128),
+               sector VARCHAR(128),
+               industry VARCHAR(128),
+               employee_count INTEGER
+            )
+            """)
+
+### Create table for stock prices
+cur.execute("""
+               CREATE TABLE stock_prices(
+               id SERIAL PRIMARY KEY,
+               date DATE,
+               name VARCHAR(128),
+               price NUMERIC,
+               delta_price NUMERIC,
+               delta_price_perc NUMERIC,
+               top_3_news VARCHAR(256),
+               news_source VARCHAR(256)
+            )
+            """)
+
+
+###############
+# ALTER TABLE #
+###############
+
+# indexes
+cur.execute("""
+                ALTER TABLE indexes
+                RENAME TO market_index
+            """)
+
+cur.execute("""
+                ALTER TABLE market_index
+                RENAME COLUMN datetime
+                TO date
+            """)
+
+cur.execute("""
+                ALTER TABLE market_index
+                ALTER COLUMN date TYPE TIMESTAMP
+            """)
+
+# company_profile
+cur.execute("""
+                ALTER TABLE company_profile
+                ALTER COLUMN date TYPE TIMESTAMP
+            """)
+
+# stock_prices
+cur.execute("""
+                ALTER TABLE stock_prices
+                RENAME TO stock_price
+            """)
+
+cur.execute("""
+                ALTER TABLE stock_price
+                ALTER COLUMN date TYPE TIMESTAMP
+            """)
+
+postgresConnection.commit()
---
+++ pg_data_insert.py
@@ -0,0 +1,57 @@
+import psycopg2
+import json
+import ast
+import helper_functions as hf
+import os
+from datetime import datetime
+import sys
+
+start = datetime.now()
+
+# Global variables
+file_path = "/Users/nikhilsawal/OneDrive/investment_portfolio/datafiles/"
+file_name = "indexes.jl"
+pg_auth = os.environ.get("PG_AUTH")
+
+# Database credentials
+conn = psycopg2.connect(host="localhost",
+                        user="postgres",
+                        dbname="investment_db",
+                        password=pg_auth)
+cur = conn.cursor()
+
+if file_name == "index.jl":
+    sql = """INSERT INTO market_index
+         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (id) DO NOTHING;"""
+elif file_name == "company_profile.jl":
+    sql = """INSERT INTO company_profile
+         VALUES (%s, %s, %s, %s, %s, %s) ON CONFLICT (id) DO NOTHING;"""
+elif file_name == "stock_prices.jl":
+    sql = """INSERT INTO stock_price
+         VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (id) DO NOTHING;"""
+else:
+    hf.slack_msg("```XXXXX {} not found in directory!!!! Check for possible renames!!```".format(file_name))
+    sys.exit()
+
+with open(file_path+file_name) as inpFile:
+
+    for i, j in enumerate(inpFile):
+        id = i+1
+        try:
+            values = (tuple([i+1] + list(ast.literal_eval(j).values())))
+            # print(values)
+            cur.execute(sql, values)
+            conn.commit()
+        except (ValueError, psycopg2.DatabaseError) as e:
+            hf.slack_msg("```{}```".format(e))
+
+duration = datetime.now()-start
+
+hf.slack_msg("""
+```
+script: {}.py,
+datafile: {},
+status: {},
+runtime: {}
+```
+""".format("pg_data_insert", file_name, "Success", duration))
