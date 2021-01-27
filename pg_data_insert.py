cb38ab8c1f6f59811d203396d1f91004968a8155
NikhilSawal <nikhil.sawal18@gmail.com>
2021-01-27 11:37:40 -0600

    Automates datapipeline by adding code that pipes data for all three datafiles to postgres immediately after the scrape

--- pg_data_insert.py
+++ pg_data_insert.py
@@ -6,52 +6,63 @@ import os
 from datetime import datetime
 import sys
 
-start = datetime.now()
+def pipe_data(file_name):
 
-# Global variables
-file_path = "/Users/nikhilsawal/OneDrive/investment_portfolio/datafiles/"
-file_name = "indexes.jl"
-pg_auth = os.environ.get("PG_AUTH")
+    start = datetime.now()
 
-# Database credentials
-conn = psycopg2.connect(host="localhost",
-                        user="postgres",
-                        dbname="investment_db",
-                        password=pg_auth)
-cur = conn.cursor()
+    # Global variables
+    file_path = "/Users/nikhilsawal/OneDrive/investment_portfolio/datafiles/"
+    file_name = file_name
+    pg_auth = os.environ.get("PG_AUTH")
 
-if file_name == "index.jl":
-    sql = """INSERT INTO market_index
-         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (id) DO NOTHING;"""
-elif file_name == "company_profile.jl":
-    sql = """INSERT INTO company_profile
-         VALUES (%s, %s, %s, %s, %s, %s) ON CONFLICT (id) DO NOTHING;"""
-elif file_name == "stock_prices.jl":
-    sql = """INSERT INTO stock_price
-         VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (id) DO NOTHING;"""
-else:
-    hf.slack_msg("```XXXXX {} not found in directory!!!! Check for possible renames!!```".format(file_name))
-    sys.exit()
+    # Database credentials
+    conn = psycopg2.connect(host="localhost",
+                            user="postgres",
+                            dbname="investment_db",
+                            password=pg_auth)
+    cur = conn.cursor()
 
-with open(file_path+file_name) as inpFile:
+    if file_name == "index.jl":
+        sql = """INSERT INTO market_index
+             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (id) DO NOTHING;"""
+    elif file_name == "company_profile.jl":
+        sql = """INSERT INTO company_profile
+             VALUES (%s, %s, %s, %s, %s, %s) ON CONFLICT (id) DO NOTHING;"""
+    elif file_name == "stock_prices.jl":
+        sql = """INSERT INTO stock_price
+             VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (id) DO NOTHING;"""
+    else:
+        hf.slack_msg("```XXXXX {} not found in directory!!!! Check for possible renames!!```".format(file_name))
+        sys.exit()
 
-    for i, j in enumerate(inpFile):
-        id = i+1
-        try:
-            values = (tuple([i+1] + list(ast.literal_eval(j).values())))
-            # print(values)
-            cur.execute(sql, values)
-            conn.commit()
-        except (ValueError, psycopg2.DatabaseError) as e:
-            hf.slack_msg("```{}```".format(e))
+    with open(file_path+file_name) as inpFile:
 
-duration = datetime.now()-start
+        for i, j in enumerate(inpFile):
+            id = i+1
 
-hf.slack_msg("""
-```
-script: {}.py,
-datafile: {},
-status: {},
-runtime: {}
-```
-""".format("pg_data_insert", file_name, "Success", duration))
+            try:
+                values = (tuple([id] + list(ast.literal_eval(j).values())))
+                cur.execute(sql, values)
+                conn.commit()
+            except (ValueError, psycopg2.DatabaseError) as e:
+                hf.slack_msg("```{}```".format(e))
+
+    duration = datetime.now()-start
+
+    hf.slack_msg("""
+    ```
+    script: {}.py,
+    datafile: {},
+    status: {},
+    runtime: {}
+    ```
+    """.format("pg_data_insert", file_name, "Success", duration))
+
+def main():
+
+    pipe_data("company_profile.jl")
+    pipe_data("index.jl")
+    pipe_data("stock_prices.jl")
+
+if __name__ == "__main__":
+    main()
