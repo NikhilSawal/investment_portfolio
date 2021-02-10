fcf1e9c28335f163d5db50c27b84390765d07850
NikhilSawal <nikhil.sawal18@gmail.com>
2021-02-10 11:38:34 -0600

    Fixes spider_marketIndex.jl to remove commas from delta_indexes.jl

--- spider_marketIndex.py
+++ spider_marketIndex.py
@@ -32,13 +32,13 @@ class IndexSpider(scrapy.Spider):
         yield {
             'datetime'          : datetime.datetime.now().strftime("%Y-%m-%d %X"),
             's&p_500'           : index[0].replace(",",""),
-            's&p_500_delta'     : delta_pattern.sub(r'\1', index[1]),
+            's&p_500_delta'     : delta_pattern.sub(r'\1', index[1]).replace(",",""),
             's&p_500_delta(%)'  : delta_perc_pattern.sub(r'\1', index[2]),
             'dow_30'            : index[3].replace(",",""),
-            'dow_30_delta'      : delta_pattern.sub(r'\1', index[4]),
+            'dow_30_delta'      : delta_pattern.sub(r'\1', index[4]).replace(",",""),
             'dow_30_delta(%)'   : delta_perc_pattern.sub(r'\1', index[5]),
             'nasdaq'            : index[6].replace(",",""),
-            'nasdaq_delta'      : delta_pattern.sub(r'\1', index[7]),
+            'nasdaq_delta'      : delta_pattern.sub(r'\1', index[7]).replace(",",""),
             'nasdaq_delta(%)'   : delta_perc_pattern.sub(r'\1', index[8]),
         }
 
