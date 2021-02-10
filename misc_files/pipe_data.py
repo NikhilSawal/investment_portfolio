80907393b929ae3bbc5c7062a4d7cf65146907ca
NikhilSawal <nikhil.sawal18@gmail.com>
2021-02-10 11:40:27 -0600

    adds script that fixes old buggy index data

--- misc_files/pipe_data.py
+++ misc_files/pipe_data.py
@@ -68,24 +68,46 @@ import re
 #             reordered = {k: j[k] for k in desired_order}
 #             json.dump(reordered, outFile)
 #             outFile.write('\n')
+#
+# ####################################################################################################
+#
+# # 02/09/2021
+# # Fixes data bug in stock_price.jl from 4th Feb through 8th Feb
+# delta_pattern = re.compile(r'[()+-]*([+-]+\d+\.\d+)[%()]*')
+#
+# with open('/Users/nikhilsawal/OneDrive/investment_portfolio/tst_file.jl','w') as outputfile:
+#     with open('/Users/nikhilsawal/OneDrive/investment_portfolio/datafiles/stock_prices.jl', 'rb') as inputfile:
+#         for index, item in enumerate(json_lines.reader(inputfile)):
+#             if index > 2518 and index <= 2650:
+#                 item['price'] = item['price'].replace(",","")
+#                 item['delta_price'] = item['delta_price'].replace(",","")
+#                 item['delta_price_perc'] = delta_pattern.sub(r'\1', item['delta_price_perc'])
+#                 # print(item)
+#                 json.dump(item, outputfile)
+#                 outputfile.write('\n')
+#             else:
+#                 json.dump(item, outputfile)
+#                 outputfile.write('\n')
+#                 pass
+
 
 ####################################################################################################
 
-# 02/09/2021
-# Fixes data bug in stock_price.jl from 4th Feb through 8th Feb
-delta_pattern = re.compile(r'[()+-]*([+-]+\d+\.\d+)[%()]*')
+# 02/10/2021
+# Fixes data bug in index.jl on 2021-02-05 08:00:03
+delta_perc_pattern = re.compile(r'(.?\d+\.\d+).+')
 
 with open('/Users/nikhilsawal/OneDrive/investment_portfolio/tst_file.jl','w') as outputfile:
-    with open('/Users/nikhilsawal/OneDrive/investment_portfolio/datafiles/stock_prices.jl', 'rb') as inputfile:
+    with open('/Users/nikhilsawal/OneDrive/investment_portfolio/datafiles/index.jl', 'rb') as inputfile:
         for index, item in enumerate(json_lines.reader(inputfile)):
-            if index > 2518 and index <= 2650:
-                item['price'] = item['price'].replace(",","")
-                item['delta_price'] = item['delta_price'].replace(",","")
-                item['delta_price_perc'] = delta_pattern.sub(r'\1', item['delta_price_perc'])
+            if index == 170:
+                item['s&p_500_delta'] = item['s&p_500_delta'].replace(",","")
+                item['dow_30_delta'] = item['dow_30_delta'].replace(",","")
+                item['nasdaq_delta'] = item['nasdaq_delta'].replace(",","")
                 # print(item)
                 json.dump(item, outputfile)
                 outputfile.write('\n')
             else:
                 json.dump(item, outputfile)
                 outputfile.write('\n')
-                pass
+                # pass
